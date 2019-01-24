0c644ff65254b2631edbabd3a269af7eb7e61ff0
Kevin Schoen <Kevinschoen05@Kevins-MacBook-Pro-2.local>
2019-01-23 20:56:02 -0500

    missed file from previous commit

--- script_login.js
+++ script_login.js
@@ -2,6 +2,5 @@ function login(){
     var username = document.getElementById("usernameInput");
     var password = document.getElementById("passwordInput");
 
-    console.log(username.value);
-    console.log(password.value);
+    
 }
